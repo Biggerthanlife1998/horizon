3c355a2a29ea8ae64040d437977b4d502c13965f
biggerthanlife1998 <biggerthanlife1998@gmail.com>
2025-08-30 22:42:59 +0100

    Add alternative build tools and force install Rollup package

---
+++ client/webpack.config.js
@@ -0,0 +1,35 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: './src/main.tsx',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM',
+  },
+};
